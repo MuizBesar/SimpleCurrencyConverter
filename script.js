5aff518c4086ba3d6f9f7359b252b9ddecba7cae
MuizBesar <125269813+MuizBesar@users.noreply.github.com>
2023-03-10 22:22:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ fetch("https://api.frankfurter.app/currencies")
 
 function display(data){
     const entries = Object.entries(data);
-    for(var i = 0; i < entries.length; i++){
+    for(let i = 0; i < entries.length; i++){
         select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
         select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
     }
